78f70b210824444265de00d53343627fb164dcff
Camila Diaz <marcdiazsan@unal.edu.co>
2021-07-22 11:40:17 -0500

    tests: change tests to work with the full workspace path stored in db
    Closes reana-hub/reana-db#94

--- reana_workflow_controller/workflow_run_manager.py
+++ reana_workflow_controller/workflow_run_manager.py
@@ -624,7 +624,7 @@ class KubernetesWorkflowRunManager(WorkflowRunManager):
             chown_workspace_cmd = "chown -R {}:{} {};".format(
                 WORKFLOW_RUNTIME_USER_UID,
                 WORKFLOW_RUNTIME_USER_GID,
-                SHARED_VOLUME_PATH + "/" + self.workflow.workspace_path,
+                self.workflow.workspace_path,
             )
             run_app_cmd = 'su {} /bin/bash -c "{}"'.format(user, base_cmd)
             full_cmd = add_group_cmd + add_user_cmd + chown_workspace_cmd + run_app_cmd
