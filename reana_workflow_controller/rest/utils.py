1d8cf8357ad67dfa35e2ac72a1098c8e7681f7dc
Tibor Šimko <tibor.simko@cern.ch>
2022-10-06 14:18:24 +0200

    rest: respect user when deleting all workflow runs
    Fixes a problem when calling `delete --include-all-runs` could have
    deleted other person's workflows by simple name matching.

--- reana_workflow_controller/rest/utils.py
+++ reana_workflow_controller/rest/utils.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of REANA.
-# Copyright (C) 2020, 2021 CERN.
+# Copyright (C) 2020, 2021, 2022 CERN.
 #
 # REANA is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -206,6 +206,7 @@ def delete_workflow(workflow, all_runs=False, workspace=False):
                     Session.query(Workflow)
                     .filter(
                         Workflow.name == workflow.name,
+                        Workflow.owner_id == workflow.owner_id,
                         Workflow.status != RunStatus.running,
                     )
                     .all()
