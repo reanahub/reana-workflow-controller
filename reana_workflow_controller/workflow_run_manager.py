9215a1e6eb597fc1c3f55b66c87fc440e83e8173
alextintin007 <alextintin007@outlook.com>
2024-04-10 10:46:48 -0500

    Submit Workflow Job with Kueue

--- reana_workflow_controller/workflow_run_manager.py
+++ reana_workflow_controller/workflow_run_manager.py
@@ -496,7 +496,7 @@ class KubernetesWorkflowRunManager(WorkflowRunManager):
             labels={
                 "reana_workflow_mode": "batch",
                 "reana-run-batch-workflow-uuid": str(self.workflow.id_),
-                "kueue.x-k8s.io/queue-name": "batch-queue-batch",
+                "kueue.x-k8s.io/queue-name": "batch-queue-batch",           # Submit Workflow Job with Kueue
             },
             namespace=REANA_RUNTIME_KUBERNETES_NAMESPACE,
         )
