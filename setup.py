8ae9ee4a9a1e316c241c531e6fc1d6d58cbbef70
Marco Donadoni <marco.donadoni@cern.ch>
2024-05-29 14:30:16 +0200

    build(python): drop support for Python 3.6 and 3.7 (#586)
    BREAKING CHANGE: drop support for Python 3.6 and 3.7
    
    Closes reanahub/reana#784

--- setup.py
+++ setup.py
@@ -94,6 +94,7 @@ setup(
             "consume-job-queue = reana_workflow_controller." "cli:consume_job_queue",
         ]
     },
+    python_requires=">=3.8",
     extras_require=extras_require,
     install_requires=install_requires,
     setup_requires=setup_requires,
